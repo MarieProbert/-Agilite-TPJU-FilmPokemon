86e7d47168b008188ca2e89b5e91d5c9c323c7b1
Baaptistee <144432551+Baaptistee@users.noreply.github.com>
2025-12-18 15:17:03 +0100

    test

---
+++ Tests/test_films_pokemon.py
@@ -0,0 +1,13 @@
+import unittest
+import os
+import sys
+
+sys.path.append(os.path.dirname(os.path.abspath(__file__)) + "/..")
+
+from Models.film import Film, Categorie
+from Models.realisateur import Realisateur
+from Models.acteur import Acteur
+from Models.pokemon import Pokemon
+from Models.pokeball import Pokeball
+from Models.dresseur import Dresseur
+
