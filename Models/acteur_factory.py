f6c30b3f7272f946a826d96caf9532e6a713149a
Baaptistee <144432551+Baaptistee@users.noreply.github.com>
2025-12-18 15:40:19 +0100

    factory

---
+++ Models/acteur_factory.py
@@ -0,0 +1,25 @@
+from Models.acteur import Acteur
+from Models.pokemon import Pokemon
+from Models.pokemon_adapter import PokemonAdapter
+
+class ActeurFactory:
+    @staticmethod
+    def creer_acteur(type_source, nom, dialogue, film=None): 
+        
+        if type_source == "humain":
+            acteur = Acteur(nom)
+            acteur.set_dialogue(dialogue)
+            if film:
+                acteur.set_film(film)
+            return acteur
+        elif type_source == "pokemon":
+            
+
+            pokemon = Pokemon(nom=nom, type1="Inconnu", type2="Inconnu", capacite=dialogue)
+            acteur_adapter = PokemonAdapter(pokemon)
+            if film:
+                acteur_adapter.set_nom(film)
+            return acteur_adapter
+        
+        else:
+            raise ValueError("Type de source inconnu pour créer un acteur")
